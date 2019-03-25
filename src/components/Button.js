e8e7c825327dd3e4fe173a24a4412fa37f4eb897
Wildan Ziaulhaq <wildanzhq@gmail.com>
2019-03-25 11:33:05 +0700

    Get context using consumer

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,13 +2,17 @@ import React from 'react';
 import LanguageContext from '../contexts/LanguageContext';
 
 class Button extends React.Component {
-    static contextType = LanguageContext;
+    renderSubmit(value) {
+        return value === 'en' ? 'Submit' : 'Tambah';
+    }
 
     render() {
-        const text = this.context === 'en' ? 'Submit' : 'Tambah';
-
         return (
-            <button className="ui button primary">{text}</button>
+            <button className="ui button primary">
+                <LanguageContext.Consumer>
+                    {value => this.renderSubmit(value)}
+                </LanguageContext.Consumer>
+            </button>
         );
     }
 }
